dfe33f9fcf7c8ded6f5d41fef6fd1b5567f18daf
Bullish Design <BullishDesignLLC@gmail.com>
2024-07-11 17:19:36 -0500

    Updating keymaps.

--- config/utils/whichkey.nix
+++ config/utils/whichkey.nix
@@ -2,7 +2,7 @@
   # TODO: Implement general mappings
   plugins.which-key = {
     enable = true;
-    ignoreMissing = true;
+    ignoreMissing = false;
     icons = {
       breadcrumb = "»";
       group = "+";
