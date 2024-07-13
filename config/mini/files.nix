60a34dadb5448bbee8fa9d50db08c003ef963255
Bullish Design <BullishDesignLLC@gmail.com>
2024-07-13 13:10:48 -0500

    Mini settings update

--- config/mini/files.nix
+++ config/mini/files.nix
@@ -8,19 +8,32 @@
       #     command = "lua require('mini.files').open()";
       #   }
       # ];
-    autoCmd = [
-      {
-        event = "User";
-        pattern = "MiniFilesWindowOpen";
-        callback = {
-          __raw = ''
-            function(args)
-              local win_id = args.data.win_id
-              vim.api.nvim_win_set_config(win_id, { border = 'rounded' })
-            end
-          '';
-        };
-      }
-    ];
+    # autoCmd = [
+    #   {
+    #     event = "User";
+    #     pattern = "MiniFilesWindowOpen";
+    #     callback = {
+    #       __raw = ''
+    #         function(args)
+    #           local win_id = args.data.win_id
+    #           vim.api.nvim_win_set_config(win_id, { border = 'rounded' })
+    #         end
+    #       '';
+    #     };
+    #   }
+    # ];
   };
+
+  keymaps = [
+      {
+        action = "<cmd>lua MiniFiles.open()<cr>";
+        key = "<leader>e";
+        options = {
+          desc = "Open File Explorer";
+        };
+        mode = [
+          "n"
+        ];
+      }
+  ];
 }
