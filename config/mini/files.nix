011158ef70e2482aa8f4dd5c3d650ddd5850feea
Bullish Design <BullishDesignLLC@gmail.com>
2024-07-13 13:19:02 -0500

    Mini settings update

--- config/mini/files.nix
+++ config/mini/files.nix
@@ -1,39 +1,16 @@
 {
-  # programs.nixvim = {
-    plugins.mini.modules.files = {
-      # keys = [
-      #   {
-      #     mode = "n";
-      #     keys = "<leader>e";
-      #     command = "lua require('mini.files').open()";
-      #   }
-      # ];
-    # autoCmd = [
-    #   {
-    #     event = "User";
-    #     pattern = "MiniFilesWindowOpen";
-    #     callback = {
-    #       __raw = ''
-    #         function(args)
-    #           local win_id = args.data.win_id
-    #           vim.api.nvim_win_set_config(win_id, { border = 'rounded' })
-    #         end
-    #       '';
-    #     };
-    #   }
-    # ];
-  };
+  plugins.mini.modules.files = {};
 
   keymaps = [
-      {
-        action = "<cmd>lua MiniFiles.open()<cr>";
-        key = "<leader>e";
-        options = {
-          desc = "Open File Explorer";
-        };
-        mode = [
-          "n"
-        ];
-      }
+    {
+      action = "<cmd>lua MiniFiles.open()<cr>";
+      key = "<leader>e";
+      options = {
+        desc = "Open File Explorer";
+      };
+      mode = [
+        "n"
+      ];
+    }
   ];
 }
