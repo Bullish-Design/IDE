bd7f8844c95d2ed2bd1862c8db272d5367521238
Bullish Design <BullishDesignLLC@gmail.com>
2024-08-01 14:47:07 -0500

    Updated keymaps.

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -183,7 +183,7 @@
       mode = "n";
       key = "<leader><Tab><Tab><Tab>";
       # action = "<M-L>";
-      action = "<cmd>nvim_feedkeys(nvim_replace_termcodes('<M-L>', true, false, true), 'n', false)<cr>";
+      action = "<cmd>lua nvim_feedkeys(nvim_replace_termcodes('<M-L>', true, false, true), 'n', false)<cr>";
       options.desc = "Next Window";
     }
 
