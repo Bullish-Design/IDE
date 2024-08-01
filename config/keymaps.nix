4b04fa177a971628b0c8e56e3850b0e972c63f2b
Bullish Design <BullishDesignLLC@gmail.com>
2024-08-01 14:29:54 -0500

    Updated keymaps.

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -182,8 +182,8 @@
     {
       mode = "n";
       key = "<leader><Tab><Tab><Tab>";
-      # action = "<M-L>";
-      action = "<cmd>lua nvim_feedkeys(nvim_replace_termcodes("<M-L>", true, false, true), 'n', false)<cr>";
+      action = "<M-L>";
+      # action = "<cmd>:call nvim_feedkeys(nvim_replace_termcodes("<M-L>", true, false, true), 'n', false)<cr>";
       options.desc = "Next Window";
     }
 
@@ -191,7 +191,8 @@
       mode = "n";
       key = "<leader><Tab><Tab>`";
       # action = "<M-H>";
-      action = "<cmd>lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<M-H>", true, false, true), 'n', false)<cr>";
+      # action = "<cmd>lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<M-H>", true, false, true), 'n', false)<cr>";
+      action = ":lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<M-H>", true, false, true), 'n', false)<cr>";
       ooptions.desc = "Previous Window";
     }
 
