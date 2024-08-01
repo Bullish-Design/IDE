1ba19ae2765f08e4e112ecfc1221f9736ce299cb
Bullish Design <BullishDesignLLC@gmail.com>
2024-08-01 14:57:18 -0500

    Updated keymaps.

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -183,7 +183,9 @@
       mode = "n";
       key = "<leader><Tab><Tab><Tab>";
       # action = "<M-L>";
-      action = "<cmd>lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<M-L>', true, false, true), 'n', false)<cr>";
+      # action = "<cmd>lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<M-L>', true, false, true), 'n', false)<cr>";
+
+      action = "<cmd>! tmux next-window<cr>";
       options.desc = "Next Window";
     }
 
