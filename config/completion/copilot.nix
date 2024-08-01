758cf5560a38763d2c3121a003e0f50e05326fc0
Bullish Design <BullishDesignLLC@gmail.com>
2024-08-01 18:39:10 -0500

    Configuring copilot and autocomplete.

--- config/completion/copilot.nix
+++ config/completion/copilot.nix
@@ -1,34 +1,41 @@
 {
   plugins.copilot-lua = {
     enable = true;
+    # panel = {
+    #   enabled = false;
+    #   autoRefresh = true;
+    #   keymap = {
+    #     jumpPrev = "[[";
+    #     jumpNext = "]]";
+    #     accept = "<Tab>";
+    #     refresh = "gr";
+    #     open = "<M-CR>";
+    #   };
+    #   layout = {
+    #     position = "bottom"; # | top | left | right
+    #     ratio = 0.4;
+    #   };
+    # };
+    # suggestion = {
+    #   enabled = true;
+    #   autoTrigger = true;
+    #   debounce = 75;
+    #   keymap = {
+    #     accept = "<M-l>";
+    #     acceptWord = false;
+    #     acceptLine = false;
+    #     next = "<M-]>";
+    #     prev = "<M-[>";
+    #     dismiss = "<C-]>";
+    #   };
+    # };
     panel = {
       enabled = false;
-      autoRefresh = true;
-      keymap = {
-        jumpPrev = "[[";
-        jumpNext = "]]";
-        accept = "<Tab>";
-        refresh = "gr";
-        open = "<M-CR>";
-      };
-      layout = {
-        position = "bottom"; # | top | left | right
-        ratio = 0.4;
-      };
     };
     suggestion = {
-      enabled = true;
-      autoTrigger = true;
-      debounce = 75;
-      keymap = {
-        accept = "<M-l>";
-        acceptWord = false;
-        acceptLine = false;
-        next = "<M-]>";
-        prev = "<M-[>";
-        dismiss = "<C-]>";
-      };
+      enabled = false;
     };
+    
     filetypes = {
       python = true;
       nix = true;
