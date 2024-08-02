dccb5c0ddbeb7180184dfce696a5d6c352e7c25f
Bullish Design <BullishDesignLLC@gmail.com>
2024-08-01 21:14:47 -0500

    Configuring copilot and autocomplete.

--- config/completion/copilot.nix
+++ config/completion/copilot.nix
@@ -1,56 +1,56 @@
-{
-  plugins.copilot-lua = {
-    enable = true;
-    # panel = {
-    #   enabled = false;
-    #   autoRefresh = true;
-    #   keymap = {
-    #     jumpPrev = "[[";
-    #     jumpNext = "]]";
-    #     accept = "<Tab>";
-    #     refresh = "gr";
-    #     open = "<M-CR>";
-    #   };
-    #   layout = {
-    #     position = "bottom"; # | top | left | right
-    #     ratio = 0.4;
-    #   };
-    # };
-    # suggestion = {
-    #   enabled = true;
-    #   autoTrigger = true;
-    #   debounce = 75;
-    #   keymap = {
-    #     accept = "<M-l>";
-    #     acceptWord = false;
-    #     acceptLine = false;
-    #     next = "<M-]>";
-    #     prev = "<M-[>";
-    #     dismiss = "<C-]>";
-    #   };
-    # };
-    panel = {
-      enabled = false;
-    };
-    suggestion = {
-      enabled = false;
-    };
+# {
+#   plugins.copilot-lua = {
+#     enable = true;
+#     # panel = {
+#     #   enabled = false;
+#     #   autoRefresh = true;
+#     #   keymap = {
+#     #     jumpPrev = "[[";
+#     #     jumpNext = "]]";
+#     #     accept = "<Tab>";
+#     #     refresh = "gr";
+#     #     open = "<M-CR>";
+#     #   };
+#     #   layout = {
+#     #     position = "bottom"; # | top | left | right
+#     #     ratio = 0.4;
+#     #   };
+#     # };
+#     # suggestion = {
+#     #   enabled = true;
+#     #   autoTrigger = true;
+#     #   debounce = 75;
+#     #   keymap = {
+#     #     accept = "<M-l>";
+#     #     acceptWord = false;
+#     #     acceptLine = false;
+#     #     next = "<M-]>";
+#     #     prev = "<M-[>";
+#     #     dismiss = "<C-]>";
+#     #   };
+#     # };
+#     panel = {
+#       enabled = false;
+#     };
+#     suggestion = {
+#       enabled = false;
+#     };
     
-    filetypes = {
-      python = true;
-      nix = true;
-      lua = true;
-      yaml = false;
-      markdown = true;
-      help = false;
-      gitcommit = false;
-      gitrebase = false;
-      hgcommit = false;
-      svn = false;
-      cvs = false;
-      # "." = false;
-    };
-    copilotNodeCommand = "node"; # Node.js version must be > 18.x
-    serverOptsOverrides = {};
-  };
-}
+#     filetypes = {
+#       python = true;
+#       nix = true;
+#       lua = true;
+#       yaml = false;
+#       markdown = true;
+#       help = false;
+#       gitcommit = false;
+#       gitrebase = false;
+#       hgcommit = false;
+#       svn = false;
+#       cvs = false;
+#       # "." = false;
+#     };
+#     copilotNodeCommand = "node"; # Node.js version must be > 18.x
+#     serverOptsOverrides = {};
+#   };
+# }
