877063b536ebb3e22f9ee4c86d63e9ddfcdb5f90
Bullish Design <BullishDesignLLC@gmail.com>
2024-08-02 21:02:12 -0500

    Configuring clipboard.

---
+++ config/utils/neoclip.nix
@@ -0,0 +1,38 @@
+{
+  plugins.neoclip = {
+    enable = true;
+    settings = {
+      # history = 10000;
+      enable_persistant_history = true;
+      content_spec_column = false;
+      default_register = "\"";
+      filter = null;
+      keys = {
+        fzf = {
+          custom = { };
+          paste = "ctrl-l";
+          paste_behind = "ctrl-h";
+          select = "default";
+        };
+        telescope = {
+          i = {
+            custom = { };
+            paste = "<c-l>";
+            paste_behind = "<c-h>";
+            select = "<cr>";
+          };
+          n = {
+            custom = { };
+            paste = "p";
+            paste_behind = "P";
+            select = "<cr>";
+          };
+        };
+      };
+      on_paste = {
+        set_reg = false;
+      };
+      preview = true;
+    };
+  };
+}
