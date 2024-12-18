e8a751356b7503678208e09981ae444741137313
Bullish Design <BullishDesignLLC@gmail.com>
2024-12-18 08:33:28 -0500

    Update

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -7,7 +7,7 @@
       enable = true;
       capabilities = "offsetEncoding =  'utf-16'";
       servers = {
-        fennel_language_server = {enable = true;};
+        #fennel_language_server = {enable = true;};
         fennel_ls = {enable = true;};
         clangd = {enable = true;};
         lua-ls = {
